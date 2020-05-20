acd8e2cf685d284fdbb1d95da3f817fe418d3f36
abangoding <abangoding@gmail.com>
2020-05-20 08:00:39 +0700

    Change placeholder form email address login

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -3,14 +3,14 @@
     <!-- Outer Row -->
     <div class="row justify-content-center">
 
-        <div class="col-lg-7">
+        <div class="col-lg-5">
 
             <div class="card o-hidden border-0 shadow-lg my-5">
                 <div class="card-body p-0">
                     <!-- Nested Row within Card Body -->
                     <div class="row">
                         <div class="col-lg">
-                            <div class="p-5">
+                            <div class="p-4">
                                 <div class="text-center">
                                     <h1 class="h4 text-gray-900 mb-4">Login Page</h1>
                                 </div>
@@ -19,7 +19,7 @@
 
                                 <form class="user" method="post" action="<?= base_url('auth'); ?>">
                                     <div class="form-group">
-                                        <input type="text" class="form-control form-control-user" id="email" name="email" placeholder="Enter Email Address..." value="<?= set_value('email'); ?>">
+                                        <input type="text" class="form-control form-control-user" id="email" name="email" placeholder="Email Address" value="<?= set_value('email'); ?>">
                                         <?= form_error('email', '<small class="text-danger pl-3">', '</small>'); ?>
                                     </div>
                                     <div class="form-group">
